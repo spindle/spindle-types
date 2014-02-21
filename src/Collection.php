e21edb2ef963b6ea735d3e31e76f5509d2197da4
Hiraku NAKANO <hiraku@tojiru.net>
2014-02-22 08:37:26 +0900

    update docblocks

--- src/Collection.php
+++ src/Collection.php
@@ -1,18 +1,23 @@
 <?php
 /**
- * Spindle\Types\Collection
- * 厳密な配列
+ * Spindle\Types
  *
  * @license MIT
  */
 namespace Spindle\Types;
 
+/**
+ * 厳密な配列を実現するクラス。
+ * 添え字は0から始まり、必ず順番が守られる。
+ * 文字列の添え字は許可されない。
+ */
 abstract class Collection extends \SplFixedArray
 {
     /**
-     * 自動伸長させる
+     * SplFixedArrayは固定長になってしまうため、自動伸長機能を追加
      * @param int $offset
      * @param mixed $value
+     * @return void
      */
     function offsetSet($offset, $value)
     {
@@ -25,16 +30,13 @@ abstract class Collection extends \SplFixedArray
         return parent::offsetSet($offset, $value);
     }
 
-    function __set($name, $value)
-    {
-        throw new \RuntimeException('__set() is not allowed');
-    }
-
-    function __get($name)
-    {
-        throw new \RuntimeException('__get() is not allowed');
-    }
-
+    /**
+     * SplFixedArray固定ではなく、継承した子クラスを返すように拡張
+     *
+     * @param array $array 元にする配列
+     * @param bool $save_indexes なるべく元の添え字を保持しようとするかどうか
+     * @return static
+     */
     static function fromArray(array $array, $save_indexes=true)
     {
         $splFixedArray = \SplFixedArray::fromArray($array, $save_indexes);
@@ -45,5 +47,21 @@ abstract class Collection extends \SplFixedArray
         }
 
         return $self;
+    }
+
+    /**
+     * getterは許可しない
+     */
+    function __get($name)
+    {
+        throw new \RuntimeException('__get() is not allowed');
+    }
+
+    /**
+     * setterは許可しない
+     */
+    function __set($name, $value)
+    {
+        throw new \RuntimeException('__set() is not allowed');
     }
 }
